a21dfc0bc5615e286a8c4d4809afda6ed2b35ddd
Artem <kohanartem@gmail.com>
2023-11-12 19:24:07 +0300

    Initial Commit

---
+++ TestImplementationSwiftUI/TestImplementationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestImplementationSwiftUIApp.swift
+//  TestImplementationSwiftUI
+//
+//  Created by Артем Кохан on 12.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestImplementationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
